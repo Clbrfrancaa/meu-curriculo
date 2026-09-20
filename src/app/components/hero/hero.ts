33806280548634505e77ee8e98704005d1fe7c7c
Cleber França <cleber.francaa12@gmail.com>
2026-09-20 00:37:43 -0300

    ainda não achei o erro

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -18,6 +18,6 @@ export class Hero {
   // Avatar tech com referência a Lobo/Cyberpunk via API do Dicebear
   readonly avatarUrl = signal(
     // 'https://api.dicebear.com/9.x/bottts-neutral/svg?seed=CrimsonWolf&backgroundColor=161118'
-    'eu.jpg'
+    '/eu.jpg'
   );
 }
